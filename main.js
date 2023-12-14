c19eebe938e4a00267d20e71e677419ccdbaf957
melovitor <vitormelobatista@hotmail.com>
2023-12-14 11:18:32 -0300

    Passando parametro de som

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function playSoundPom(){
-    document.querySelector('#som_tecla_pom').play()
+function playSounds(idAudio){
+    document.querySelector(idAudio).play()
 }
 
 const teclaList = document.querySelectorAll('.tecla')
@@ -7,6 +7,12 @@ const teclaList = document.querySelectorAll('.tecla')
 let conter = 0
 
 while (conter < teclaList.length){
-    teclaList[conter].onclick = playSoundPom
+    const key =  teclaList[conter]
+    const instrument = key.classList[1]
+    const idAudio = `#som_${instrument}`
+
+    key.onclick = () => {
+        playSounds(idAudio)
+    }
     conter ++
 }
